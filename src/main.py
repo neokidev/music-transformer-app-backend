48e7e984dcc76f794398979d035bf9673e71430d
ot07 <ot07neoki@gmail.com>
2022-05-10 20:08:39 +0900

    🚧 テストのためにルートのエンドポイントを追加

--- src/main.py
+++ src/main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
 @app.get("/examples/bach_format0")
 async def examples_bach_format0():
     return bach_format0
